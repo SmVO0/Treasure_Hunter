055d86072ccbdc7cccc6933a9edeb85910da09ef
samuilovcharov <samuil.ovcharov01@gmail.com>
2025-01-07 11:43:36 +0200

    Adding player images and method to load them

--- src/Entity/BaseEntity.java
+++ src/Entity/BaseEntity.java
@@ -1,12 +1,20 @@
 package Entity;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class BaseEntity {
 
     public int x;
     public int y;
     public int speed;
 
-    public BaseEntity() {
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
 
-    }
+    public BaseEntity() {}
+
+
 }
--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -3,7 +3,9 @@ package Entity;
 import Main.GamePanel;
 import Main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.IOException;
 
 public class Player extends BaseEntity {
 
@@ -13,29 +15,49 @@ public class Player extends BaseEntity {
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
-        this.setDefaultValues();
+        setDefaultValues();
     }
 
     public void setDefaultValues() {
-        this.x = 100;
-        this.y = 100;
-        this.speed = 4;
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+
+    }
+
+    public void getPlayerImage(){
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/back_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/back_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/front_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/front_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/right_2.png"));
+
+        } catch (IOException e){ e.printStackTrace(); }
     }
 
     public void update() {
         if(this.keyH.upPressed){
+            direction = "up";
             this.y -= this.speed;
         }
 
         if(this.keyH.downPressed){
+            direction = "down";
             this.y += this.speed;
         }
 
         if(this.keyH.leftPressed){
+            direction = "left";
             this.x -= this.speed;
         }
 
         if(this.keyH.rightPressed){
+            direction = "right";
             this.x += this.speed;
         }
     }
