aa1523414b58d942404431841256a309e8dfa285
samuilovcharov <samuil.ovcharov01@gmail.com>
2025-01-09 20:56:43 +0200

    Added new tile img

--- src/Tile/TileManager.java
+++ src/Tile/TileManager.java
@@ -5,6 +5,7 @@ import Main.GamePanel;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
+import java.util.Objects;
 
 public class TileManager {
 
@@ -23,18 +24,23 @@ public class TileManager {
     public void getTileImage() {
         try{
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+            tile[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/grass.png")));
 
             tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+            tile[1].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/wall.png")));
 
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+            tile[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/wall2.png")));
+
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/water.png")));
         } catch (IOException e){ e.printStackTrace(); }
     }
 
     public void draw(Graphics2D g2) {
         g2.drawImage(tile[0].image, 0, 0, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(tile[1].image, 48, 0, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(tile[2].image, 96, 0, gp.tileSize, gp.tileSize, null);
 
     }
 }
